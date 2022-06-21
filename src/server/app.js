9411eaa5ff86ce7e2bbbd17dd6f103e5b01606a7
joepuzzo <jgpuzzo2@gmail.com>
2022-06-20 20:04:09 -0700

    added vercel config

--- src/server/app.js
+++ src/server/app.js
@@ -13,7 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const createApp = ({ corsConfig }) => {
-  // Create Express applicationn
+  // Create Express application
   const app = express();
 
   // Health endpoint
